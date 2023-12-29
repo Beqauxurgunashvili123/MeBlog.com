9a84536482aef7ed8b3faa4227c740a9045dedef
Beqauxurgunashvili123 <154613520+Beqauxurgunashvili123@users.noreply.github.com>
2023-12-29 22:07:19 +0400

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,2 +1,2 @@
 from ext import app
-from route import index, add_article, update_article, edit_article, delete_article, register, login, about, sponsor, howto, search
+from route import index, add_article, update_article, edit_article, delete_article, register, login, about, sponsor, howto, search, unauthorized
